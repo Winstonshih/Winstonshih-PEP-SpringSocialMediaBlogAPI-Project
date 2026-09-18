6391590ec30e8df4fa3d741f4e0b9879d3b4f1cf
Winstonshih <Winstonshih@github.com>
2026-09-18 19:30:19 +0000

    Starting on project

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,12 +1,49 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
  * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
+@RequestMapping("/api/users")
 public class SocialMediaController {
+  AccountService accountService;
+  MessageService messageService;
+  /**
+   * No params constructor that instantiates AccountService and MessageService objects that will be used to construct 
+   * eendpoints for REST API.
+   */
+  public SocialMediaController()
+  {
+      this.accountService= new AccountService();
+      this.messageService=new MessageService();
+  }
+  
+  @RequestMapping(value="/register", method = RequestMethod.POST)
+    public @ResponseBody Account register(@RequestBody Account newUser) {
+       
+        Account newAccount=accountService.createAccount(newUser);
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public @ResponseBody Account login(@RequestBody Account existingUser) {
+        // Logic to authenticate user login
+        return null;
+    }
 
 }
