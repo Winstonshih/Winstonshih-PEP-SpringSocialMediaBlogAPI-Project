11cd27fa85eef6643b7980e6b78cdd7d853ad5b5
Winstonshih <Winstonshih@github.com>
2026-09-19 17:24:09 +0000

    Test run: 24/28 passed, 4 failed, 0 skipped - 2026-09-19T17:24:09.608Z

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -100,7 +100,7 @@ public class SocialMediaController {
         return ResponseEntity.status(400).build();
     }
     @RequestMapping(value="/accounts/{accountId}/messages", method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity<List<Message>> retrieveAllPostsByUser(@PathVariable("postedBy") Integer id) {
+    public @ResponseBody ResponseEntity<List<Message>> retrieveAllPostsByUser(@PathVariable("accountId") Integer id) {
         return ResponseEntity.status(200).body(messageService.getAllPostsByUser(id));
     }
 }
