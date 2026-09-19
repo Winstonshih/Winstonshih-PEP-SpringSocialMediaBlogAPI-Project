88bde452aaa73250ff21f58cdd9bc8955afa4c8f
Winstonshih <Winstonshih@github.com>
2026-09-19 17:15:32 +0000

    Test run: 22/28 passed, 6 failed, 0 skipped - 2026-09-19T17:15:32.405Z

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -92,10 +92,10 @@ public class SocialMediaController {
         return ResponseEntity.status(200).body(messageService.deleteAllPostsById(id));
     }
     @RequestMapping(value="/messages/{messageId}", method = RequestMethod.PATCH)
-    public @ResponseBody ResponseEntity<Integer> updateAllPostsById(@PathVariable("messageId") Integer id, @PathVariable("messageText") String text) {
-        if(messageService.updateAllPostsById(id, text)!=null)
+    public @ResponseBody ResponseEntity<Integer> updateAllPostsById(@PathVariable("messageId") Integer id, @RequestBody Message message) {
+        if(messageService.updateAllPostsById(id, message.getMessageText())!=null)
         {
-          return ResponseEntity.status(200).body(messageService.updateAllPostsById(id, text));
+          return ResponseEntity.status(200).body(messageService.updateAllPostsById(id, message.getMessageText()));
         }
         return ResponseEntity.status(400).build();
     }
