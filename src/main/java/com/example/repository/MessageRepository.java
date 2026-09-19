01b77ee3a6b5b4764db47c4bfb1dfd56088f3757
Winstonshih <Winstonshih@github.com>
2026-09-19 16:36:48 +0000

    Test run: 18/28 passed, 10 failed, 0 skipped - 2026-09-19T16:36:48.866Z

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface MessageRepository extends JpaRepository<Message, Integer>{
   Optional<Message> findById(Integer id);
-  Optional<Message> deleteAllPostsById(Integer id);
-  Optional<Message> updateAllPostsById(Integer id);
+  
 }
