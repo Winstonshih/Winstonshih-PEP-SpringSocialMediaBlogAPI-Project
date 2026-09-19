04f037c9ba82fad755a7f899a40cdff8161cb8e5
Winstonshih <Winstonshih@github.com>
2026-09-19 17:57:30 +0000

    Test run: 25/28 passed, 3 failed, 0 skipped - 2026-09-19T17:57:30.642Z

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,6 +1,6 @@
 package com.example.repository;
 import com.example.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface AccountRepository extends JpaRepository<Account, Long>{
+public interface AccountRepository extends JpaRepository<Account, Integer>{
   Account findAccountByUsername(String username);
 }
