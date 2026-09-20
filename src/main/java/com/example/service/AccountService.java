516aa2e6d693b4fa9c2e5bd3d628033336c1489a
Winstonshih <Winstonshih@github.com>
2026-09-20 21:00:45 +0000

    Updated AccountService comments.

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -8,11 +8,20 @@ import com.example.repository.AccountRepository;
 @Service
 public class AccountService {
   private AccountRepository accountRepository;
+  /**
+   * Constructor for accountRepository objects.
+   * @param accountRepository accountRepository object
+   */
   @Autowired
   public AccountService(AccountRepository accountRepository)
   {
     this.accountRepository=accountRepository;
   }
+  /**
+   * Method to register accounts by checking if username is already used.
+   * @param account account that will bee created.
+   * @return registered account if username is null or null if username is not null.
+   */
   public Account register(Account account)
   {
     Account newUser=accountRepository.findAccountByUsername(account.getUsername());
@@ -22,6 +31,11 @@ public class AccountService {
     }
     return null;
   }
+  /**
+   * Login method that checks if username is in database.
+   * @param account existing user account
+   * @return existing user account if it exists or null if not.
+   */
   public Account login(Account account)
   {
     Account existingUser=accountRepository.findAccountByUsername(account.getUsername());
