7d1e8feea9a0fa723a1141fd5ab523a502273f73
Winstonshih <Winstonshih@github.com>
2026-09-20 20:57:53 +0000

    Updated AccountRepository comments.

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -2,5 +2,10 @@ package com.example.repository;
 import com.example.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Integer>{
+  /**
+   * Finds an account by its id.
+   * @param username account's username.
+   * @return account if id is valid or null if not found.
+   */
   Account findAccountByUsername(String username);
 }
